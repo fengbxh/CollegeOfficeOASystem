dce331c07be26cad08892cd17cf819b02b5da02f
bixihua <527218901@qq.com>
2019-04-02 14:59:49 +0800

    0.0.5

--- src/main/java/cn/edu/usts/controller/RoomInofoController.java
+++ src/main/java/cn/edu/usts/controller/RoomInofoController.java
@@ -1,8 +1,32 @@
 package cn.edu.usts.controller;
 
+import cn.edu.usts.model.RoomInfoModel;
+import cn.edu.usts.pojo.PageBean;
+import cn.edu.usts.services.impl.RoomInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("room")
 public class RoomInofoController {
+    @Autowired
+    private RoomInfoService roomInfoService;
 
+    //分页查询所有的房间信息
+    @RequestMapping(value = "/findRoomInfoByPage", method = RequestMethod.POST)
+    @ResponseBody
+    public PageBean<RoomInfoModel> findRoomInfoByPage(@RequestParam(value="currentPage",defaultValue="1",required=false)int currentPage) {
+        try {
+            PageBean<RoomInfoModel> pageBean = roomInfoService.findRoomInfoByPage(currentPage);
+            return pageBean;
+        } catch (Exception ex) {
+            return null;
+        }
+    }
 }
